fa4222094b5c476fa2ae08e81f54c9629d14c889
Naibelis Freitez <nfreitezc@gmail.com>
2022-03-17 02:16:01 -0300

    se agrego cart y listcontainer

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+import './ItemListContainer.css';
+
+function ItemListContainer({ greeting }) {
+  return (
+    <>
+      <h1 className="saludo">{greeting}</h1>
+    </>
+  );
+}
+export default ItemListContainer;
