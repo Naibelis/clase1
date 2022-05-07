0636612ff9233a68472d34a2c0713ccfc88aa987
Naibelis Damalis <naibelis@MacBook-Pro-de-Naibelis.local>
2022-05-07 01:58:26 -0300

    agregado de ordenes y actualizacion de items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,28 @@ import { useEffect, useState } from "react";
 import { Button, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
+import { addDoc, collection, doc, getFirestore, updateDoc } from "firebase/firestore";
+
 
 function Cart() {
     const { cartItems, clear, removeFromCart } = useCartContext();
     const [total, setTotal] = useState();
+
+    function comprar() {
+        console.log({ cartItems })
+        const db = getFirestore();
+        const ordersCollection = collection(db, "orders");
+        let order = { items: [], quantity: 0 };
+        cartItems.forEach((item) => {
+            order.items.push(item.item);
+            order.quantity = order.quantity + item.quantity;
+
+            const itemDoc = doc(db, "items", item.item.id);
+            updateDoc(itemDoc, { stock: item.item.stock - item.quantity });
+        });
+        addDoc(ordersCollection, order).then(({ id }) => { return console.log({ orderId: id }) })
+        clear();
+    }
 
     useEffect(() => {
         let total = 0
@@ -35,8 +53,9 @@ function Cart() {
                             </ListGroupItem>
                         </ListGroup>)
                         )}
-                        <h2>Total:  ${ total } </h2>
+                        <h2>Total:  ${total} </h2>
                         <Button variant="warning" onClick={clear} className="w-25 my-2">Vaciar carrito</Button>
+                        <Button variant="success" onClick={comprar} className="w-25 my-2">Comprar</Button>
                     </Col>
                 }
             </Row>
