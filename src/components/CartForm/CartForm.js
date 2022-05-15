fdc75fcc33576a32393c692d126fad390983cb82
Naibelis Freitez <nfreitezc@gmail.com>
2022-05-15 02:25:33 -0300

    Agregado nuevo componente para formulario

---
+++ src/components/CartForm/CartForm.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Button, Col, FloatingLabel, Form, Row } from "react-bootstrap";
+
+function CartForm({ validated, submitHandler, formData, setFormData }) {
+
+    const handleOnChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    return <>
+        <Form noValidate validated={validated} onSubmit={submitHandler}>
+            <Row>
+                <Col>
+                    <FloatingLabel className="mb-3" controlId="nombre" label="Nombre">
+                        <Form.Control required type="text" value={formData.nombre} onChange={handleOnChange} placeholder="Nombre" />
+                    </FloatingLabel>
+                </Col>
+                <Col>
+                    <FloatingLabel className="mb-3" controlId="apellido" label="Apellido">
+                        <Form.Control required type="text" value={formData.apellido} onChange={handleOnChange} placeholder="Apellido" />
+                    </FloatingLabel>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FloatingLabel className="mb-3" controlId="telefono" label="Número de teléfono">
+                        <Form.Control required type="number" value={formData.telefono} onChange={handleOnChange} placeholder="Número de teléfono" />
+                    </FloatingLabel>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FloatingLabel className="mb-3" controlId="email" label="Correo electrónico">
+                        <Form.Control value={formData.email} onChange={handleOnChange} required type="email" placeholder="Correo electrónico" />
+                    </FloatingLabel>
+                </Col>
+                <Col>
+                    <FloatingLabel className="mb-3" controlId="confirmEmail" label="Confirme su correo electrónico">
+                        <Form.Control value={formData.confirmEmail} onChange={handleOnChange} required type="email" placeholder="Confirme su correo electrónico" />
+                    </FloatingLabel>
+                </Col>
+            </Row>
+            <Button variant="success" type="submit" className="w-50 my-2">Realizar mi compra</Button>
+        </Form>
+    </>
+}
+
+export default CartForm;
