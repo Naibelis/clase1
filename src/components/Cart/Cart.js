aac2b9fb00cf3756bfc90dfd64a8a54230b4d4eb
Naibelis Freitez <nfreitezc@gmail.com>
2022-05-15 02:27:26 -0300

    Agregado formulario para realizar compra y mensaje final con id de orden

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,61 @@
+import { addDoc, collection, doc, getFirestore, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Button, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { Alert, Button, Col, Container, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext";
-import { addDoc, collection, doc, getFirestore, updateDoc } from "firebase/firestore";
+import CartForm from "../CartForm/CartForm";
 
 
 function Cart() {
     const { cartItems, clear, removeFromCart } = useCartContext();
     const [total, setTotal] = useState();
+    const [validated, setValidated] = useState(false);
+    const [order, setOrder] = useState();
+    const [emailInvalid, setEmailInvalid] = useState(false);
 
-    function comprar() {
-        console.log({ cartItems })
-        const db = getFirestore();
-        const ordersCollection = collection(db, "orders");
-        let order = { items: [], quantity: 0 };
-        cartItems.forEach((item) => {
-            order.items.push(item.item);
-            order.quantity = order.quantity + item.quantity;
+    const [formData, setFormData] = useState({
+        nombre: '',
+        apellido: '',
+        telefono: '',
+        email: '',
+        confirmEmail: '',
+    });
 
-            const itemDoc = doc(db, "items", item.item.id);
-            updateDoc(itemDoc, { stock: item.item.stock - item.quantity });
-        });
-        addDoc(ordersCollection, order).then(({ id }) => { return console.log({ orderId: id }) })
-        clear();
+    function comprar(event) {
+        const form = event.currentTarget;
+        event.preventDefault();
+        setValidated(true);
+        if (formData.email != formData.confirmEmail) {
+            return setEmailInvalid(true);
+        }
+
+        if (form.checkValidity() === true) {
+            setEmailInvalid(false);
+            event.stopPropagation();
+            let order = {
+                nombre: formData.nombre,
+                apellido: formData.apellido,
+                telefono: formData.telefono,
+                email: formData.email,
+                items: [],
+                quantity: 0,
+                createdAt: new Date()
+            };
+
+            const db = getFirestore();
+            const ordersCollection = collection(db, "orders");
+            cartItems.forEach((item) => {
+                order.items.push(item.item);
+                order.quantity = order.quantity + item.quantity;
+
+                const itemDoc = doc(db, "items", item.item.id);
+                updateDoc(itemDoc, { stock: item.item.stock - item.quantity });
+            });
+            addDoc(ordersCollection, order).then(({ id }) => {
+                return setOrder({ id });
+            })
+            clear();
+        }
     }
 
     useEffect(() => {
@@ -34,32 +67,60 @@ function Cart() {
     }, [cartItems]);
 
     return <>
+
         <Container className="my-3">
             <Row className="justify-content-center">
-                {cartItems.length <= 0 ?
-                    <Col md={10}>
-                        <h1>No hay items en el carrito</h1>
-                        <Link to="/">Volver</Link>
-                    </Col> :
-                    <Col md={10}>
-                        <h1>Carrito</h1>
-                        {cartItems.map((cartItem) =>
-                        (<ListGroup key={cartItem}>
-                            <ListGroupItem>
-                                <p>{cartItem.item.title}</p>
-                                <p>Precio: ${cartItem.item.price}</p>
-                                <p>Cantidad: {cartItem.quantity}</p>
-                                <Button id={cartItem.item.id} variant="danger" onClick={() => removeFromCart(cartItem.item.id)} className="w-25 my-2">X</Button>
-                            </ListGroupItem>
-                        </ListGroup>)
-                        )}
-                        <h2>Total:  ${total} </h2>
-                        <Button variant="warning" onClick={clear} className="w-25 my-2">Vaciar carrito</Button>
-                        <Button variant="success" onClick={comprar} className="w-25 my-2">Comprar</Button>
-                    </Col>
-                }
+                <Col lg={8}>
+                    <h1>Carrito</h1>
+                    {cartItems.length <= 0
+                        ?
+                        <>
+                            <p>No hay items en el carrito</p>
+                            <Link to="/">Volver</Link>
+                        </>
+                        :
+                        <>
+                            <Button variant="warning" onClick={clear} className="w-25 my-2 mr-2">Vaciar carrito</Button>
+                            {cartItems.map((cartItem) =>
+                            (<ListGroup key={cartItem.item.id}>
+                                <ListGroupItem>
+                                    <p>{cartItem.item.title}</p>
+                                    <p>Precio: ${cartItem.item.price}</p>
+                                    <p>Cantidad: {cartItem.quantity}</p>
+                                    <Button id={cartItem.item.id} variant="danger" onClick={() => removeFromCart(cartItem.item.id)} className="my-2">X</Button>
+                                </ListGroupItem>
+                            </ListGroup>)
+                            )}
+                            <h2 className="mt-2">Total:  ${total} </h2>
+                        </>
+                    }
+                </Col>
+            </Row>
+            <Row className="mt-5 justify-content-center">
+                <Col lg={8}>
+                    <h2>
+                        Llene sus datos para comprar
+                    </h2>
+                    {
+                        order &&
+                        <div>
+                            <Alert key="success" variant="success">
+                                <h3>Orden realizada exitosamente!</h3>
+                                <p>Id de la orden: <b>{order.id}</b></p>
+                            </Alert>
+                        </div>
+                    }
+                    {
+                        emailInvalid &&
+                        <Alert key="danger" variant="danger">
+                            Los correos no coinciden
+                        </Alert>
+                    }
+                    {!order && <CartForm submitHandler={comprar} validated={validated} formData={formData} setFormData={setFormData} />}
+                </Col>
             </Row>
         </Container>
+
     </>
 }
 
